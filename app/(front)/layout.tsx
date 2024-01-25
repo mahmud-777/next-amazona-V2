6931507c3d2ea553d452834244d6e0a2cc5b252b
mahmud-777 <mahmud33k@gmail.com>
2024-01-25 10:59:24 +0600

    productmodel create

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -3,5 +3,10 @@ export default function FrontLayout({
 }: {
   children: React.ReactNode
 }) {
-  return <main className="flex-grow container mx-auto px-4">{children}</main>
+  return(
+    <main className="flex-grow container mx-auto px-4">
+    {children}
+  </main>
+  ) 
+  
 }
