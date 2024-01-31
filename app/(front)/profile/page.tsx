ecd5e37cd579097e66d9c6d0dbd06927ada3a1d6
mahmud-777 <mahmud33k@gmail.com>
2024-01-31 20:16:03 +0600

    create profile page

---
+++ app/(front)/profile/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+import Form from "./Form";
+
+export const metadata: Metadata = {
+  title: "Profile"
+}
+
+export default async function Profile(){
+
+  return (
+    <>
+      <Form />
+    </>
+  )
+}
